54b3c8616b4e4ce03dd6553abc2fb2100f09e48d
Baig-fatema <mirzafatmabaig1012@gmail.com>
2024-11-03 17:19:45 +0530

    update the hugry.py

--- hugry.py
+++ hugry.py
@@ -11,4 +11,9 @@ else:
 undo uncommited changes : $ git checkout -- hugry.py
 undo commmited changes : $ git revert commit id
 reset : $ git reset commit id (from log history)
+
+Branch :
+git branch
+git checkout new_branch name     (create new barch)
+git log       (check commit history)
 '''    
