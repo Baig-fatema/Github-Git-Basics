86bdf4a38314682e11e0ab943185d15e6dee8ba2
Baig-fatema <mirzafatmabaig1012@gmail.com>
2024-11-03 11:55:38 +0530

    Updated the hugry.py 2

--- hugry.py
+++ hugry.py
@@ -1,6 +1,13 @@
 is_hugry=input("Are you hugry?")
 if is_hugry.lower() == "yes":
     print("eat Samosa")
+    print("eat Pizza")
 else:
     print("Do complete your homework")    
     
+
+
+'''
+undo uncommited changes : $ git checkout -- hugry.py
+undo commmited changes : 
+'''    
